867d512eab15345f5fce118aa40ffa22014df953
huyvu0607 <vudh1092@ut.edu.vn>
2025-08-19 16:16:41 +0700

    feat: add reservation detail page

--- ParkingReservationSystem/ParkingReservationSystem/obj/Debug/net8.0/ParkingReservationSystem.AssemblyInfo.cs
+++ ParkingReservationSystem/ParkingReservationSystem/obj/Debug/net8.0/ParkingReservationSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParkingReservationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef452b8e53f9435480204070c30f63115a0b67e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParkingReservationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParkingReservationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
