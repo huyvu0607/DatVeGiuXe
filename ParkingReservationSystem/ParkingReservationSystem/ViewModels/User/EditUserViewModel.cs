65b37f49bf36eb5cfa2c12b234f8d61649e154e3
dinhtuandev <tuantd1384@ut.edu.vn>
2025-08-25 19:45:08 +0700

    Add Thêm ViewModel
    Thêm Viewmodel cho reset mk,quên mk, tạo user, chỉnh sửa user, thêm serviceresult

---
+++ ParkingReservationSystem/ParkingReservationSystem/ViewModels/User/EditUserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkingReservationSystem.ViewModels.User
+{
+    public class EditUserViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Họ tên không được để trống")]
+        [Display(Name = "Họ và Tên")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email không được để trống")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Mật khẩu mới")]
+        public string Password { get; set; } // Để trống nếu không đổi
+
+        // Thêm field này để lưu mật khẩu cũ
+        public string CurrentPasswordHash { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Mật khẩu xác nhận không khớp")]
+        public string? ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng chọn vai trò")]
+        [Display(Name = "Vai trò")]
+        public string Role { get; set; }
+    }
+}
