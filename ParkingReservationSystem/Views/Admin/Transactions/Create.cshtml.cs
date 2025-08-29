ef8c06157c42eaae310ff88f33dbf4575931c14c
HieuMai-27 <hieumaitrung26@gmail.com>
2025-08-29 14:34:17 +0700

    Thêm code View

---
+++ ParkingReservationSystem/Views/Admin/Transactions/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ParkingReservationSystem.Models;
+
+namespace ParkingReservationSystem.Views.Admin.History
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ParkingReservationSystem.Models.ParkingDbContext _context;
+
+        public CreateModel(ParkingReservationSystem.Models.ParkingDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["SlotCode"] = new SelectList(_context.ParkingSlots, "SlotCode", "SlotCode");
+        ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return Page();
+        }
+
+        [BindProperty]
+        public Reservation Reservation { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Reservations.Add(Reservation);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ParkingReservationSystem/Views/Admin/Transactions/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ParkingReservationSystem.Models;
+
+namespace ParkingReservationSystem.Views.Admin.History
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ParkingReservationSystem.Models.ParkingDbContext _context;
+
+        public DeleteModel(ParkingReservationSystem.Models.ParkingDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Reservation Reservation { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Reservation = reservation;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation != null)
+            {
+                Reservation = reservation;
+                _context.Reservations.Remove(Reservation);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
